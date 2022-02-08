b8e7e4dcdd1cd2b5c9d28e372fbf8dfc958d5299
taise-hikawa <cmmkarubi@gmail.com>
2022-02-08 16:57:42 +0900

    Initial Commit

---
+++ PlayDatePicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayDatePicker
+//
+//  Created by 樋川大聖 on 2022/02/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayDatePicker/PlayDatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayDatePickerApp.swift
+//  PlayDatePicker
+//
+//  Created by 樋川大聖 on 2022/02/08.
+//
+
+import SwiftUI
+
+@main
+struct PlayDatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
